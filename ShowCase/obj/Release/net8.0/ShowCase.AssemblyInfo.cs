fa171f49010a6281a07f58c4bb66f23b8fa129b8
Stephanie Santos <ssanto43@my.centennialcollege.ca>
2025-03-31 14:42:56 -0400

    Added nuget config file

--- ShowCase/obj/Release/net8.0/ShowCase.AssemblyInfo.cs
+++ ShowCase/obj/Release/net8.0/ShowCase.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShowCase")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8dd82fbce204f72e9acb5bc10eb84f3539f7709")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShowCase")]
--- StringLibrary/obj/Debug/net8.0/StringLibrary.AssemblyInfo.cs
+++ StringLibrary/obj/Debug/net8.0/StringLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StringLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5263b15c89e478d907cbae3ba8d4baac071b6907")]
 [assembly: System.Reflection.AssemblyProductAttribute("StringLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StringLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
